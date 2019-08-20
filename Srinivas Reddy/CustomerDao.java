2a5bc73aac822e7c0a5b7128e8ab66b43598b3e5
manisri562 <srinu.acceptor@gmail.com>
2019-08-20 23:43:16 +0530

    Add files via upload

---
+++ Srinivas Reddy/CustomerDao.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package my.bookstore.core.daos.impl;
+
+import de.hybris.platform.servicelayer.internal.dao.AbstractItemDao;
+
+
+/**
+ * @author srinivasR
+ *
+ */
+public class CustomerDao extends AbstractItemDao
+{
+
+}
